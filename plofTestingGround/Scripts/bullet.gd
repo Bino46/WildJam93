b9c4b2cef32310bc4101d42082ed07c1999eb0da
PLOFINBOOTS <luckylloop@gmail.com>
2026-05-11 14:00:35 +0200

    Adios nombros magicos

--- plofTestingGround/Scripts/bullet.gd
+++ plofTestingGround/Scripts/bullet.gd
@@ -1,11 +1,18 @@
 extends PoolObject2D
 
+@export_group("Tween settings")
+@export var travel_duration: float = 0.2
+@export var final_scale: Vector2 = Vector2(0.5, 0.5)
+
+const INITIAL_SCALE = Vector2.ONE
+
 func launch(target_pos: Vector2, start_pos: Vector2):
 	self.global_position = start_pos
-	self.scale = Vector2(1, 1)
+	self.scale = INITIAL_SCALE
+	
 	var tween = create_tween()
-	tween.tween_property(self, "global_position", target_pos, 0.2)
-	tween.parallel().tween_property(self, "scale", Vector2(0.5, 0.5), 0.2)
+	tween.tween_property(self, "global_position", target_pos, travel_duration)
+	tween.parallel().tween_property(self, "scale", final_scale, travel_duration)
 	
 	await tween.finished
 	#Check collision ?
