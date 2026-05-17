55d82899220f70c94e25e74e8fe8ceb222a2cc21
PLOFINBOOTS <luckylloop@gmail.com>
2026-05-17 18:07:51 +0200

    c'est pas l'école qui m'a dicté mes codes non non

--- plofTestingGround/Scripts/volumeslider.gd
+++ plofTestingGround/Scripts/volumeslider.gd
@@ -5,6 +5,8 @@ var bus_name: String = "Master"
 var bus_index: int
 
 func _ready() -> void:
+	value = 0.5
+	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
 	bus_index = AudioServer.get_bus_index(bus_name)
 	value_changed.connect(_on_value_changed)
 	
