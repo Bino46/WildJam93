ca7418bc491db69f5f2237489c557741a32b4cb6
PLOFINBOOTS <luckylloop@gmail.com>
2026-05-17 17:43:27 +0200

    blblb

---
+++ ecran_fin.gd
@@ -0,0 +1,15 @@
+extends Control
+
+var main_menu = "res://main_menu.tscn"
+
+
+func appear():
+	self.visible = true
+	get_tree().paused = true
+	get_score()
+	
+func get_score():
+	$ColorRect/Label2.text = "SCORE :" + str(Global.score)
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file(main_menu)
