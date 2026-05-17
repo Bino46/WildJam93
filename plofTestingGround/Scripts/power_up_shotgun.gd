b657d6d16292cc4fddb898bc50282d4893c9da7f
PLOFINBOOTS <luckylloop@gmail.com>
2026-05-17 15:28:19 +0200

    power up bug ?

--- plofTestingGround/Scripts/power_up_shotgun.gd
+++ plofTestingGround/Scripts/power_up_shotgun.gd
@@ -4,9 +4,14 @@ extends Node2D
 
 func _ready() -> void:
 	$AnimatedSprite2D.play("default")
+	$Area2D/CollisionShape2D.disabled = true
 	
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if(area.get_collision_layer_value(2)):
 		player.switch_shotgun()
 		queue_free()
 	
+
+
+func _on_timer_timeout() -> void:
+	$Area2D/CollisionShape2D.disabled = false
